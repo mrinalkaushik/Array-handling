cf7bb8a8a72d51a29177a76f3d66b3bfb294c021
mrinal kaushik <mrinalrocks24x7gmail.com>
2022-11-16 18:19:55 +0530

    ADD: print the elements of an array

---
+++ Array.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.day3project;
+
+public class Array {
+	 public static void main(String[] args) {
+	        int a[] = {1,2,3,4,5,6,7,8,9 };
+	        for (int i = 0; i < a.length; i++)
+	            System.out.println(a[i]);
+
+	    }
+}
